b0f37ea96cab6dab8409a85c5c1b9109d499406a
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-04-03 07:50:38 -0400

    feat: agentkit package builds both esm and cjs

--- typescript/agentkit/jest.config.cjs
+++ typescript/agentkit/jest.config.cjs
@@ -2,6 +2,15 @@ const baseConfig = require("../jest.config.base.cjs");
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
   coveragePathIgnorePatterns: ["node_modules", "dist", "docs", "index.ts"],
   coverageThreshold: {},
 };
